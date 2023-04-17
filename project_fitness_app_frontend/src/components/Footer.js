335818cc7cbef77b4534fe2afda625a2247b5d42
Amitkumar Radheshyam Yadav <yadav.am@northeastern.edu>
2023-04-17 04:02:56 -0400

    Update Footer.js

--- project_fitness_app_frontend/src/components/Footer.js
+++ project_fitness_app_frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
     <Stack gap="30px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
       <img src={Logo} alt="logo" style={{ width: '120px', height: '80px', opacity:'80%' }} />
     </Stack>
-    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by Team Celtics</Typography>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️</Typography>
   </Box>
 );
 
